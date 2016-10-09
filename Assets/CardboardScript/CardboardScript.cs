90086572c4da44a2fb0742683a815e862a5c8ddc
pitercito <pitercito1990@gmail.com>
2016-10-09 11:31:48 +0200

    ADD Player(Jump) and Wall

---
+++ Assets/CardboardScript/CardboardScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardScript : MonoBehaviour {
+	public Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Jump ();
+	}
+
+	void Jump(){
+		if (Input.GetButtonDown("Jump"))
+			player.velocity = new Vector3(0, 5, 0);
+	}
+}
